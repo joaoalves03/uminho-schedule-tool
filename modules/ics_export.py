5a3953cc22a658688a4eb5397d49c73917c94382
João Alves <joao.2003.couto@gmail.com>
2025-10-15 21:46:51 +0100

    feat: allow overriding days in ICS files

--- modules/ics_export.py
+++ modules/ics_export.py
@@ -9,18 +9,27 @@ class IcsExportModule(BaseExportModule):
     """
     The ICS export module supports the following config options:
         - file_name: Exported file name
+        - override_file: Replace data on existing ICS file (useful for updating a single day/week)
     """
 
-    file_name = "out.ics"
+    file_name = "./export/out.ics"
+    override_file = None
 
     def __init__(self, config: dict):
         super().__init__()
         self.file_name = config["file_name"]
+        self.override_file = config["override_file"]
 
     def export(self, lessons: list[Lesson]):
         print("Exporting to ICS...")
 
-        calendar = ics.Calendar()
+        if self.override_file:
+            with open(self.override_file) as f:
+                calendar = ics.Calendar(f.read())
+                days = list(set(map(lambda l: l.start.strftime('%Y-%m-%d'), lessons)))
+                calendar.events = set([e for e in calendar.events if str(e.begin).split("T")[0] not in days])
+        else:
+            calendar = ics.Calendar()
 
         for lesson in lessons:
             event = ics.Event(
@@ -33,5 +42,5 @@ class IcsExportModule(BaseExportModule):
 
             calendar.events.add(event)
 
-        with open(f"./export/{self.file_name}", "w") as f:
+        with open(self.override_file if self.override_file else self.file_name, "w") as f:
             f.writelines(calendar.serialize_iter())
